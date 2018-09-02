ed10e1673d5e9e644f7014d5b527595a5ee388c0
Brian <bbar18@student.monash.edu>
2018-09-02 19:35:35 +1000

    XSRF token and business rules js calls

---
+++ business-rules-test.js
@@ -0,0 +1,39 @@
+var request = require("request");
+var fs = require("fs");
+
+var XSRF = '';
+
+var XSRFOptions = { method: 'GET',
+  url: 'https://bpmrulesruntimebpm-p2000319942trial.hanatrial.ondemand.com/rules-service/v1/rules/xsrf-token',
+  headers:
+   { 'cache-control': 'no-cache',
+     'x-csrf-token': 'Fetch',
+     authorization: 'Basic Zml0NDAwMi5pbnRlbGxpZ2VuY2VAZ21haWwuY29tOjIwMThGSVQ0MDAyPw==' } };
+
+
+//Get XSRF token from SAP
+request(XSRFOptions, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  XSRF = response.headers['x-csrf-token'];
+  console.log("Achieved XSRF token from SAP = " + XSRF)
+});
+
+var businessRulesOptions = { method: 'POST',
+  url: 'https://bpmrulesruntimebpm-p2000319942trial.hanatrial.ondemand.com/rules-service/v1/rules/invoke',
+  qs: { rule_service_name: 'IoTManager::TemperatureService' },
+  headers:
+   { 'cache-control': 'no-cache',
+     'content-type': 'application/json',
+     'x-csrf-token': 'A6EDE204A5979EE1E1BC027B421C1CB5' },
+  body: [
+    {   _type_: 'IoTManagerDataObjects',
+        Temperature: 55
+    } ],
+  json: true };
+
+request(businessRulesOptions, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+});
